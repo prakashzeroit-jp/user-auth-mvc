29aee58edc5020201c794350ce0fadaf91114681
praakash7542-jp <jyotimaurya7542@gmail.com>
2026-07-10 15:04:37 +0530

    updateProduct api

--- controllers/productController.js
+++ controllers/productController.js
@@ -70,4 +70,26 @@ const getProductById = async (req, res) => {
   }
 };
 
-module.exports = { createProduct, getProducts ,getProductById};
+const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body, 
+      { new: true, runValidators: true }
+    );
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: 'Product updated successfully',
+      data: product
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error', error: error.message });
+  }
+};
+
+module.exports = { createProduct, getProducts ,getProductById,updateProduct};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,10 +4,12 @@ const {
   createProduct,
   getProducts,
   getProductById,
+  updateProduct
 } = require("../controllers/productController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.post("/", protect, createProduct);
 router.get("/", getProducts);
 router.get("/:id", getProductById);
+router.put("/:id",protect,updateProduct);
 module.exports = router;
