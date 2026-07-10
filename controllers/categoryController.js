87ac31cd3020d23dd8d800d50fa795b445585d4b
praakash7542-jp <jyotimaurya7542@gmail.com>
2026-07-10 11:43:20 +0530

    deleteCategory api

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -55,6 +55,21 @@ const updateCategory = async (req, res) => {
   }
 };
 
+const deleteCategory = async (req, res) => {
+  try {
+    const category = await Category.findByIdAndDelete(req.params.id);
+    if (!category) {
+      return res.status(404).json({ message: 'Category not found' });
+    }
 
-module.exports = { createCategory ,getCategories,updateCategory};
+    res.status(200).json({
+      success: true,
+      message: 'Category deleted successfully'
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error', error: error.message });
+  }
+};
+
+module.exports = { createCategory ,getCategories,updateCategory,deleteCategory};
 
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createCategory,getCategories,updateCategory} = require('../controllers/categoryController');
+const { createCategory,getCategories,updateCategory,deleteCategory} = require('../controllers/categoryController');
 const { protect, admin } = require('../middleware/authMiddleware'); 
 
 router.post('/', protect,admin, createCategory);
 router.get('/',getCategories);
 router.put('/:id',protect,updateCategory);
+router.delete('/:id',protect,deleteCategory);
 
 module.exports = router;
